c34b8b7657f7611562c859dbb272fa92e77169db
taslima97 <antora.taslima97@gmail.com>
2023-05-04 09:30:39 +0600

    recipe info

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ app.get('/chef',(req, res)=>{
     res.send(chef)
 })
 
+app.get('/chef/:id',(req, res)=>{
+   const id = req.params.id;
+   
+   const selectedChef = chef.find(n=>n.id==id)
+   res.send(selectedChef)
+})
+
 app.listen(port,()=>{
     console.log(`Chef is running API port: ${port}`)
 })
